5a69be31919c40b06970735164af7271afbf5f23
kingct19 <kingchandler19@gmail.com>
2023-04-04 11:10:35 -0500

    Parallax And Nav Bar

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+      let scrollTop = window.pageYOffset;
+      let parallaxSections = document.querySelectorAll('.parallax-section');
+      
+      parallaxSections.forEach(function(section) {
+        let speed = 0.5;
+        let offset = section.offsetTop;
+        let newPos = (scrollTop - offset) * speed;
+        section.style.backgroundPosition = `center ${-newPos}px`;
+      });
+    });
+  });
+  
